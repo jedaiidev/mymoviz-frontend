098bff031eda07852cc4537f6d4f97edc1a23f9e
Lococo <anthony.lococo@jedaiidev.com>
2024-05-09 13:32:09 +0200

    https instead of http

--- components/Home.js
+++ components/Home.js
@@ -36,7 +36,7 @@ function Home() {
 
   // Movies list
   useEffect(() => {
-    fetch('http://mymoviz-backend-bay.vercel.app/movies')
+    fetch('https://mymoviz-backend-bay.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         const formatedData = data.movies.map(movie => {
